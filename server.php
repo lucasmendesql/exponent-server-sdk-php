43429117b44b05bc5cef57f4bf846f4e62a526b9
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-01-27 10:51:25 -0300

    acionamento do botão

--- server.php
+++ server.php
@@ -17,3 +17,23 @@ require_once __DIR__.'/vendor/autoload.php';
     
     // Notify an interest with a notification
     $expo->notify([$channelName], $notification);
+
+    if (isset($_POST["enviar"])) {
+        echo "Botão foi clicado";
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form method="post">
+            <Button name="enviar" > enviar</Button>
+        </form>
+</body>
+</html>
